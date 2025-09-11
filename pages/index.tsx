4471ae3e7b793c1c18208ce881835ee2c801fb45
jds1985 <jimsaenz1985@yahoo.com>
2025-09-11 11:37:11 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+import GlitchBackground from '../components/GlitchBackground'
+import { Hero } from '../components/Hero'
+
+export default function Home(){
+  return (
+    <Layout>
+      <GlitchBackground />
+      <Hero />
+    </Layout>
+  )
+}
